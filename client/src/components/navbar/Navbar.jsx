768dc750bd42f3afaf149a832b40f4917a62d385
Alizhan <alizhan.tortayev@fizmat.kz>
2023-02-22 12:56:16 +0600

    l7 reg_form-2

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Navbar.css'
 import Logo from '../assets/img/navbar-logo.svg'
+import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="container">
-                <img src={Logo} alt="" className="navbar_logo"/>
-                <div className="navbar_header">MERN CLOUD</div>
-                <div className="navbar_login">Войти</div>
-                <div className="navbar_registration">Регистрация</div>
+                <img src={Logo} alt="" className="navbar__logo"/>
+                <div className="navbar_header">TORT CLOUD</div>
+                <div className="navbar_login"><NavLink to="/login">Войти</NavLink></div>
+                <div className="navbar_registration"><NavLink to="/registration">Регистрация</NavLink></div>
             </div>
-
         </div>
     );
 };
